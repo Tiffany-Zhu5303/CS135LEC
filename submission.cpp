9daee74a133e129a75a1b5e62bec515786b483e2
Tiffany Zhu <75339798+Tiffany-Zhu5303@users.noreply.github.com>
2022-02-02 02:31:02 +0000

    comment added

--- submission.cpp
+++ submission.cpp
@@ -1,3 +1,12 @@
+/*
+Author: Tiffany Zhu 
+Course: CSCI-135
+Instructor: Tong Yi 
+Assignment: HW E1.7
+
+To have an input of three different names and to print each name on a separate line.
+
+*/
 #include <iostream>
 using namespace std;
 
