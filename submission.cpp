ab9d44bbe1e2d0c3fbbe1735f9a1cfee49f90fc7
Tiffany Zhu <75339798+Tiffany-Zhu5303@users.noreply.github.com>
2022-02-02 02:21:20 +0000

    file rename

---
+++ submission.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string name1, name2, name3;
+
+  cout << "Enter the first name ";
+  cin >> name1;
+
+  cout << "Enter the second name ";
+  cin >> name2;
+
+  cout << "Enter the third name ";
+  cin >> name3;
+
+  cout << name1 << "\n" << name2 << "\n" << name3;
+
+  return 0;
+}
