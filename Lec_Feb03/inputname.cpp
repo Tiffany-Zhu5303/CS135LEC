c147f8b3d686d00068737a03569e8d969e6d1e3d
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2022-02-07 08:46:19 -0500

    week 1

---
+++ Lec_Feb03/inputname.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //getline(cin, name);
+
+  string name;
+  cout << "What's you name?" << endl;
+  getline(cin, name);
+  cout << name;
+  
+  return 0;
+}
